0da5bfa0c531a27c7292cff2fd59efb4a05dc576
svinstvo-og <maxim12.05.2006koval@gmail.com>
2025-11-02 01:34:08 +0100

    Deleted all 6m-related code

--- src/main/java/varta/controller/IngestionController.java
+++ src/main/java/varta/controller/IngestionController.java
@@ -1,16 +1,11 @@
 package varta.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import varta.model.pgsql.Sixm;
 import varta.service.NormalizationService;
-import varta.service.TransactionRaw6mService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Optional;
 
 @RequestMapping("api/ingestion/")
 @RestController
@@ -20,26 +15,8 @@ public class IngestionController {
     final
     NormalizationService normalizationService;
 
-    final
-    TransactionRaw6mService transactionRaw6mService;
-
-    public IngestionController(TransactionRaw6mService transactionRaw6mService, NormalizationService normalizationService) {
-        this.transactionRaw6mService = transactionRaw6mService;
+    public IngestionController(NormalizationService normalizationService) {
         this.normalizationService = normalizationService;
-    }
-
-    @GetMapping({"/test/datasets/6m/{id}"})
-    public Sixm fetchTransaction(@PathVariable Integer id) {
-        log.info("Accepted 'Get transaction by {} id' request", id);
-        Optional<Sixm> transaction = transactionRaw6mService.getTransactionById(id);
-        if (transaction.isPresent()) {
-            log.info("Transaction found: {}", transaction.get().getId());
-            return transaction.get();
-        }
-        else {
-            log.warn("Transaction not found: {}", id);
-            return null;
-        }
     }
 
     @GetMapping("/test/financial_transaction/{id}")
--- src/main/java/varta/repository/pgsql/TransactionRaw6mRepository.java
+++
@@ -1,9 +0,0 @@
-package varta.repository.pgsql;
-
-import varta.model.pgsql.Sixm;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TransactionRaw6mRepository extends JpaRepository<Sixm, Integer> {
-}
