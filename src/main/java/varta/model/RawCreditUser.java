0450ba0580cd26d5b4bdab55b6db0be472b071bb
svinstvo-og <maxim12.05.2006koval@gmail.com>
2025-11-02 01:20:48 +0100

    Added new entities

---
+++ src/main/java/varta/model/RawCreditUser.java
@@ -0,0 +1,37 @@
+package varta.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "credit_user")
+@Getter
+@Setter
+public class RawCreditUser {
+
+    @Id
+    private Long id;
+
+    private Integer age;
+    private Integer gender;
+    private String job;
+    private Integer wage;
+
+    @Column(columnDefinition = "TEXT")
+    private String card;
+
+    private Integer abnormal;
+
+    @Column(name = "abnormal_state", columnDefinition = "JSONB")
+    private String abnormalState;
+
+    @Column(name = "user_no")
+    private String userNo;
+
+    @Column(name = "loc_id")
+    private String locId;
+}
---
+++ src/main/java/varta/model/mysql/RawCreditCard.java
@@ -0,0 +1,54 @@
+package varta.model.mysql;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "credit_card")
+public class RawCreditCard {
+
+    @Id
+    @Column(name = "card_id")
+    private Long cardId;
+
+    @Column(name = "owner_type")
+    private String ownerType;
+
+    @Column(name = "owner_id")
+    private Long ownerId;
+
+    @Column(name = "C4")
+    private String cardIdentifier;
+
+    @Column(name = "C5")
+    private String cardType;
+
+    @Column(name = "C6")
+    private String cardProductCode;
+
+    @Column(name = "C7")
+    private String cardNickname;
+
+    @Column(name = "C8")
+    private String cardFeatureFlag;
+
+    @Column(name = "C9")
+    private String locationId;
+
+    @Column(name = "C10")
+    private String branchCode;
+
+    @Column(name = "C11")
+    private String fullLocationCode;
+
+    private Integer abnormal;
+
+    @Column(name = "abnormal_state", columnDefinition = "JSONB")
+    private String abnormalState;
+}
