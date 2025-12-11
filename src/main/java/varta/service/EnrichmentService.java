3842533e2f4fbccd80dea5d5d117cbc743f008f8
svinstvo-og <maxim12.05.2006koval@gmail.com>
2025-12-11 21:43:54 +0100

    Added enriched transaction dto

---
+++ src/main/java/varta/service/EnrichmentService.java
@@ -0,0 +1,16 @@
+package varta.service;
+
+import io.lettuce.core.support.caching.RedisCache;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+import varta.model.pgsql.CreditTransaction;
+
+@Service
+public class EnrichmentService {
+
+//    private RedisTemplate<>
+
+    public EnrichmentService() {
+
+    }
+}
