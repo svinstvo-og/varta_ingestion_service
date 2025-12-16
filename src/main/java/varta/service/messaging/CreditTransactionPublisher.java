112675ef2d32ca48d7767190c65a87dbf275cc42
svinstvo-og <maxim12.05.2006koval@gmail.com>
2025-12-16 23:59:29 +0100

    Added Credit Transaction publisher class for Kafka straming

--- src/main/java/varta/service/messaging/CreditTransactionPublisher.java
+++ src/main/java/varta/service/messaging/CreditTransactionPublisher.java
@@ -5,28 +5,26 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
-import varta.dto.FatTransactionDto;
+import varta.model.pgsql.CreditTransaction;
 
 @Component
 @RequiredArgsConstructor
 @Slf4j
-public class FatTransactionPublisher {
+public class CreditTransactionPublisher {
+    private final KafkaTemplate<String, CreditTransaction> kafkaTemplate;
 
-    private final KafkaTemplate<String, FatTransactionDto> kafkaTemplate;
-
-    @Value("${spring.kafka.topic.fat-transactions}")
+    @Value("credit-transaction")
     private String topicName;
 
-    public void publish(FatTransactionDto payload) {
+    public void publish(CreditTransaction payload) {
         kafkaTemplate
                 .send(topicName, payload)
                 .whenComplete((result, ex) -> {
                     if (ex != null) {
-                        log.error("Failed to publish FatTransactionDto", ex);
+                        log.error("Failed to publish Credit Transaction", ex);
                         return;
                     }
-                    log.debug("Published FatTransactionDto to topic {} partition {} offset {}", topicName,
-                            result.getRecordMetadata().partition(), result.getRecordMetadata().offset());
+                    log.debug("Published Credit Transaction to topic {} partition {} offset {}", topicName, result.getRecordMetadata().partition(), result.getRecordMetadata().offset());
                 });
     }
 }
